db15cffe828c008f9ed0e546a73e45a0deac8f6b
unknown <djongit@hotmail.com>
2023-09-28 13:07:25 +0100

    initial commit plus file structure created

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import {SearchBar} from '../SearchBar/SearchBar';
+function App() {
+  return (
+    <div className="App">
+        <SearchBar />
+
+
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
