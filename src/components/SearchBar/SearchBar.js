b952779e4138ab60ce45825f4751237931ac6533
unknown <djongit@hotmail.com>
2023-10-01 14:49:23 +0100

    Components complete, readMe to do

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 
+import "./SearchBar.css";
 
-export const SearchBar = () => {
-    return (
-        <>
-            <input></input>
-        </>
-    )
-}
+const SearchBar = (props) => {
+  const [term, setTerm] = useState("");
+
+  const handleTermChange = useCallback((event) => {
+    setTerm(event.target.value);
+  }, []);
+
+  const search = useCallback(() => {
+    props.onSearch(term);
+  }, [props.onSearch, term]);
+
+  return (
+    <div className="SearchBar">
+      <input placeholder="Enter A Song Title" onChange={handleTermChange} />
+      <button className="SearchButton" onClick={search}>
+        SEARCH
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
